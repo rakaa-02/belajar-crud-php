38ba25f9fda7c69c54c56b9cddf3127bcf9e9f20
Adyaraka Sholehudin Syahputra <rakatrizyy@gmail.com>
2026-09-21 09:30:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Belajar CRUD PHP Native" 
---
+++ layout/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+        </div>
+        <div class="float-end">
+            <p>
+                Crafted with
+                <span class="text-danger"></span>
+                by <a href="https://saugi.me">raka</a>
+            </p>
+        </div>
+    </div>
+</footer>
